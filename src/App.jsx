198350406af26efc4b0874017254c42147bb4297
Prashant Adhikari <prashantadhikari440@gmail.com>
2025-02-17 22:43:45 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <>
+    <h2>Main Content</h2>
+    <p>This is main Content.</p>
+    </>
+  )
+}
+
+export default MainContent
