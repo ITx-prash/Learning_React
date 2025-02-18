806c8781bcb2ca0722492bc171f51585eec77222
Prashant Adhikari <prashantadhikari440@gmail.com>
2025-02-18 21:16:02 +0545

    Tailwind setup

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function MainContent() {
   return (
     <>
-    <h2>Main Content</h2>
-    <p>This is main Content.</p>
+      <section className="border border-gray-700 rounded-md mx-5 px-3 py-1">
+        <h2>Main Content</h2>
+        <p>This is main Content. Lorem ipsum dolor sit amet.</p>
+      </section>
     </>
-  )
+  );
 }
 
-export default MainContent
+export default MainContent;
